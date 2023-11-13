0ecc177b5f0d00ce7cfb5b4a74f7312073728baf
Shivam Bhardwaj <Shivambhardwaj8585@gmail.com>
2023-11-14 01:44:53 +0530

    worked on mailer, verifyemail and signup page

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect()
 
@@ -30,6 +31,9 @@ export async function POST(request: NextRequest) {
         // Saving the new user after hashing the password.
         const savedUser = await newUser.save();
         console.log(savedUser);
+
+        // Send an email.
+        await sendEmail({email, emailType: "VERIFY", userID: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully!",
---
+++ src/helpers/mailer.ts
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer"
+import User from "@/models/userModel"
+import bcryptjs from "bcryptjs"
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+    try {
+        // Create the Hashed Token
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+        // Update the user.
+
+        if (emailType === "VERIFY") {
+            await User.findByIdAndUpdate(userId, {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000,
+            })
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId, {
+                forgotPasswordToken: hashedToken,
+                forgotPasswordTokenExpiry: Date.now() + 3600000,
+            })
+        }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "59434b2ae02eff",
+              pass: "cea4c51b83901d"
+            }
+          });
+
+        const mailOptions = {
+            from: "shivambhardwaj8585@gmail.com",
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}"Here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your email"} </p>`
+        }
+
+        const mailResponse = await transport.sendMail(mailOptions)
+
+        return mailResponse
+
+    } catch (error: any) {
+        throw new Error(error.message)
+    }
+}
